daee5ab5319b744ed0cdaae515fd9c9838ae3e89
Aleksandr <Lunatik_@bk.ru>
2017-04-08 20:30:00 +0500

    read word process

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String resources[] = new String[] {"1.txt", "2.txt", "3.txt"};
+
+        Thread t[] = new Thread[resources.length];
+
+        for (int i=0; i<resources.length; i++) {
+            t[i] = new Searcher(resources[i]);
+        }
+
     }
 }
--- src/com/company/Searcher.java
+++ src/com/company/Searcher.java
@@ -1,17 +1,37 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created by admin on 08.04.2017.
  */
 public class Searcher extends Thread {
+    String res;
 
-    public Searcher() {
+    public Searcher(String res) {
+        this.res = res;
         Thread t = new Thread(this);
         t.start();
     }
 
     public void run() {
-        //
+        File file = new File(res);
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            scanner.next();
+            /*while (scanner.hasNext()) {
+                System.out.println("1");
+                System.out.println(scanner.next());
+            }*/
+
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
+            e.printStackTrace();
+        }
     }
 
 }
