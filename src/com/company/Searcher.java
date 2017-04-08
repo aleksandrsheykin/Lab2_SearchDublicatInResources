5173d93ff6b12b3409f1b11f5beeeec51df5e9f9
Aleksandr <Lunatik_@bk.ru>
2017-04-08 19:35:11 +0500

    first commit

---
+++ src/com/company/Searcher.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by admin on 08.04.2017.
+ */
+public class Searcher extends Thread {
+
+    public Searcher() {
+        Thread t = new Thread(this);
+        t.start();
+    }
+
+    public void run() {
+        //
+    }
+
+}
