bd6d213c29573c362098298d3138c16d72771cb3
Aleksandr <Lunatik_@bk.ru>
2017-04-08 23:27:00 +0500

    add in set process

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.Set;
+
 public class Main {
 
+    public static volatile Set<String> wordSet;
+
     public static void main(String[] args) {
-        String resources[] = new String[] {"1.txt", "2.txt", "3.txt"};
+        String resources[] = new String[] {"1.txt"/*, "2.txt", "3.txt"*/};
 
         Thread t[] = new Thread[resources.length];
 
--- src/com/company/Searcher.java
+++ src/com/company/Searcher.java
@@ -3,6 +3,10 @@ package com.company;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.company.Main.wordSet;
 
 /**
  * Created by admin on 08.04.2017.
@@ -18,15 +22,30 @@ public class Searcher extends Thread {
 
     public void run() {
         File file = new File(res);
+        Matcher m;
 
         try {
             Scanner scanner = new Scanner(file);
+            String word;
 
-            scanner.next();
-            /*while (scanner.hasNext()) {
-                System.out.println("1");
-                System.out.println(scanner.next());
-            }*/
+            while (scanner.hasNext()) {
+                word = scanner.next();
+
+                if (Pattern.compile("[a-zA-Z]+").matcher(word).matches()) {
+                    System.out.println("File "+file+" has eng word");
+                    return;
+                }
+                word = word.replaceAll("[^а-яА-Я]+", "");
+
+                //synchronized (wordSet) {
+                    if (!wordSet.add(word)) {
+                        System.out.println("word ''"+word+"'' repeated in file "+res);
+                        return;
+                    }
+                //}
+
+                System.out.println(word);
+            }
 
         } catch (FileNotFoundException e) {
             System.out.println("file not found");
